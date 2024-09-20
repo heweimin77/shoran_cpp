131ed0e1abf8d8fe31ace0604ab0e1879aebe76c
heweimin <heweimin@zhejianglab.com>
2024-09-20 15:36:33 +0800

    problem3300, 3201~3210

---
+++ session4000/problem3300/p3206.numberOfAlternatingGroups.cpp
@@ -0,0 +1,81 @@
+﻿#include <gtest/gtest.h>
+
+using namespace std;
+
+namespace {
+class AutoRun {
+public:
+    AutoRun() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+
+} autorun;
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& colors) {
+        int ans = 0;
+        colors.push_back(colors[0]);
+        colors.push_back(colors[1]);
+        for (int i = 1; i < colors.size()-1; ++i) {
+            if (colors[i] != colors[i-1] && colors[i] != colors[i+1]) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+};
+class Solution2 {
+public:
+    int numberOfAlternatingGroups(vector<int>& colors) {
+        int ans = 0, n = colors.size();
+        if (colors[0] != colors[1] && colors[0] != colors.back()) ++ans;
+        if (colors.back() != colors[0] && colors.back() != colors[n-2]) ++ans;
+        for (int i = 1; i < n-1; ++i) {
+            if (colors[i] != colors[i-1] && colors[i] != colors[i+1]) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+};
+
+class P3206Test : public testing::Test
+{
+public:
+    virtual void SetUp()
+    {
+    }
+    virtual void TearDown()
+    {
+    }
+};
+
+TEST_F(P3206Test, Test1)
+{
+    Solution s;
+    vector<int> colors = {1,1,1};
+    auto actual = s.numberOfAlternatingGroups(colors);
+    decltype(actual) expect = 0;
+    EXPECT_EQ(expect, actual);
+}
+TEST_F(P3206Test, Test2)
+{
+    Solution s;
+    vector<int> colors = {0,1,0,0,1};
+    auto actual = s.numberOfAlternatingGroups(colors);
+    decltype(actual) expect = 3;
+    EXPECT_EQ(expect, actual);
+}
+TEST_F(P3206Test, Test3)
+{
+    Solution s;
+    vector<int> colors = {0, 0, 1};
+    auto actual = s.numberOfAlternatingGroups(colors);
+    decltype(actual) expect = 1;
+    EXPECT_EQ(expect, actual);
+}
+}
+
+
---
+++ session4000/problem3300/p3209.countSubarrays.cpp
@@ -0,0 +1,78 @@
+﻿#include <gtest/gtest.h>
+
+using namespace std;
+
+namespace {
+static auto autorun = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+} ();
+
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int k) {
+        unordered_map<int,long long> nows;
+        long long ans = 0;
+        for (int x : nums) {
+            if ((x & k)!=k) {
+                nows.clear();
+                continue;
+            }
+            unordered_map<int,long long> nexts;
+            nexts[x] = 1;
+            for (auto &n : nows) {
+                int nk = n.first & x;
+                if ((nk & k) != k) continue;
+                nexts[nk] += n.second;
+            }
+            ans += nexts[k];
+            nows = std::move(nexts);
+        }
+        return ans;
+    }
+};
+
+class P3209Test : public testing::Test
+{
+public:
+    virtual void SetUp()
+    {
+    }
+    virtual void TearDown()
+    {
+    }
+};
+
+TEST_F(P3209Test, Test1)
+{
+    Solution s;
+    vector<int> nums = {1,1,1};
+    int k = 1;
+    auto actual = s.countSubarrays(nums, k);
+    decltype(actual) expect = 6;
+    EXPECT_EQ(expect, actual);
+}
+TEST_F(P3209Test, Test2)
+{
+    Solution s;
+    vector<int> nums = {1,1,2};
+    int k = 1;
+    auto actual = s.countSubarrays(nums, k);
+    decltype(actual) expect = 3;
+    EXPECT_EQ(expect, actual);
+}
+TEST_F(P3209Test, Test3)
+{
+    Solution s;
+    vector<int> nums = {1,2,3};
+    int k = 2;
+    auto actual = s.countSubarrays(nums, k);
+    decltype(actual) expect = 2;
+    EXPECT_EQ(expect, actual);
+}
+
+}
+
+
